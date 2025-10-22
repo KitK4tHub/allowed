3fb85a6d48b60e048abe3a78ce8575b64a97b230
KitK4t Hub <KitK4tHub@gmail.com>
2025-10-22 09:28:31 -0300

    Adicionando meka_js à lista de permitidos

--- users/allow.lua
+++ users/allow.lua
@@ -1,12 +1,13 @@
 return {
-    "Breno010L",
-    "shawnlovebazuka",
-    "ziovxoo",
-    "Kay_g4m4",
-    "gabi6kkjj",
-    "beatrizz2317",
-    "ACNDIEGO",
-    "saitama9rsm",
-    "Faxcarioca",
-    "RUANRODRIGO121121"
+    "Breno010L"",
+    "shawnlovebazuka"",
+    "ziovxoo"",
+    "Kay_g4m4"",
+    "gabi6kkjj"",
+    "beatrizz2317"",
+    "ACNDIEGO"",
+    "saitama9rsm"",
+    "Faxcarioca"",
+    "RUANRODRIGO121121",
+    "meka_js"
 }
