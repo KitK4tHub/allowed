574346919a8f5c3b8ae8317a9b761d5514d05824
KitK4t Hub <KitK4tHub@gmail.com>
2025-10-22 09:17:37 -0300

    Adicionando ruanrodrigo121121 à lista de permitidos

--- users/allow.lua
+++ users/allow.lua
@@ -1,11 +1,12 @@
 return {
-    "Breno010L",
-    "shawnlovebazuka",
-    "ziovxoo",
-    "Kay_g4m4",
-    "gabi6kkjj",
-    "beatrizz2317",
-    "ACNDIEGO",
-    "saitama9rsm",
-    "Faxcarioca"
+    "Breno010L"",
+    "shawnlovebazuka"",
+    "ziovxoo"",
+    "Kay_g4m4"",
+    "gabi6kkjj"",
+    "beatrizz2317"",
+    "ACNDIEGO"",
+    "saitama9rsm"",
+    "Faxcarioca",
+    "ruanrodrigo121121"
 }
